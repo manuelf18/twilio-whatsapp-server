c6bac0b8ded80f53efe80c79328c8f88dfd8a689
Manuel Fernandez <manuel.fernandez@balboadevelopers.com>
2019-01-16 16:23:25 -0500

    added express

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
 
 
 const response = new MessagingResponse();
 response.message('This is message 1 of 2.');
 response.message('This is message 2 of 2.');
 
-console.log(response.toString());
+app.get('/', (req, res) => {
+    res.type('application/xml');
+    res.send(response.toString());
+});
+
+app.listen(port, () => { console.log(`Escuchando en el puerto ${port}...`) });
